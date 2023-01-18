4927ced050b822f9f5fb4a865e1f03450e76470d
downinahill <brian.hill.0023@gmail.com>
2023-01-18 16:33:32 -0500

    changed the title and got rid of the recursive function that was preventing sanity from running

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Down in a Hill Designs</title>
       </Head>
       <header>
         <Navbar />
